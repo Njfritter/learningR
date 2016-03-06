a5b19fa4ac30fc39174e4df789d7a8ed376d1a3e
njfritter <nathan.fritter@yahoo.com>
2016-03-05 17:52:01 -0800

    Finished regression analysis for S & P 500 and NYSE

--- stockMarketProject.R
+++ stockMarketProject.R
@@ -32,6 +32,8 @@
 	fedFunds <- ts(timeSeries$fedFunds, start=c(1995, 1), freq=12)
 	capUtilization <- ts(timeSeries$capUtilization, start=c(1995, 1), freq=12)
 	unemploymentRate <- ts(timeSeries$unemploymentRate, start=c(1995, 1), freq=12)
+	dividends <- ts(timeSeries$sp_500Dividends, start=c(1995, 1), freq=12)
+	inflation <- ts(timeSeries$inflation, start=c(1995, 1), freq=12)
 	sp_500 <- ts(timeSeries$sp_500, start=c(1995, 1), freq=12)
 	nasdaq <- ts(timeSeries$nasdaq, start=c(1995, 1), freq=12)
 	nyse <- ts(timeSeries$nyse, start=c(1995, 1), freq=12)
@@ -84,23 +86,23 @@
 	print("Printing summary of initial regression on economic indicators vs NASDAQ ---------------------------------------------------")
 	summary(nasdaq_fit1)
 
-# Looking at the summary for the fit, m2 has the lowest p value
+# Looking at the summary for the fit, m2 has the highest p value
 # Using the process of backwards elimination, we will drop m2
 
 	nasdaq_fit2 <- lm(nasdaq ~ m1 + consumerSentiment + imports + oilPrices + ppi + exports + cpi + unemploymentRate + fedFunds + capUtilization + nyse, data = timeSeries)
 	summary(nasdaq_fit2)
 
-# Looking again, we find that ppi has the lowest p value; it will be dropped
+# Looking again, we find that ppi has the highest p value; it will be dropped
 
 	nasdaq_fit3 <- lm(nasdaq ~ m1 + consumerSentiment + imports + oilPrices + exports + cpi + unemploymentRate + fedFunds + capUtilization + nyse, data = timeSeries)
 	summary(nasdaq_fit3)
 
-# Next variable with lowest p value that we will drop is: imports
+# Next variable with highest p value that we will drop is: imports
 
 	nasdaq_fit4 <- lm(nasdaq ~ m1 + consumerSentiment + oilPrices + exports + cpi + unemploymentRate + fedFunds + capUtilization + nyse, data = timeSeries)
 	summary(nasdaq_fit4)
 
-# Next variable to be dropped with lowest p value: oil prices
+# Next variable to be dropped with highest p value: oil prices
 
 	nasdaq_fit5 <- lm(nasdaq ~ m1 + consumerSentiment + exports + cpi + unemploymentRate + fedFunds + capUtilization + nyse, data = timeSeries)
 	print("Printing summary of final regression on economic indicators vs NASDAQ ---------------------------------------------------")
@@ -116,7 +118,7 @@
 
 	sp500_fit1 <- lm(sp_500 ~ ., data=timeSeries)
 	print("Printing summary of initial regression on economic indicators vs S & P 500 ---------------------------------------------------")
-	summary(sp500fit1)
+	summary(sp500_fit1)
 
 # BIC Method
 	
@@ -124,36 +126,42 @@
 	drop1(sp500_fit1, k=log(n))
 
 # Using the BIC method, we will find and eliminate the variable with the lowest AIC value 
-# According to the summary, cpi has the lowest AIC value
-# cpi will be dropped
+# According to the summary, US GDP has the lowest AIC value
+# US GDP will be dropped
 
-	sp500_fit2 <- lm(sp_500 ~ . - cpi,  data=timeSeries)
+	sp500_fit2 <- lm(sp_500 ~ . - gdp_us,  data=timeSeries)
 	summary(sp500_fit2)
 	drop1(sp500_fit2, k=log(n))
 
+# According to the drop1 summary, cpi has the lowest AIC value and will be dropped
+
+	sp500_fit3 <- lm(sp_500 ~ . - gdp_us - cpi,  data=timeSeries)
+	summary(sp500_fit3)
+	drop1(sp500_fit3, k=log(n))
+
 # According to the drop1 summary, m2 has the lowest AIC value and will be dropped
 
-	sp_500fit3 <- lm(sp_500 ~ . - cpi - m2, data=timeSeries)
-	summary(sp_500fit3)
-	drop1(sp_500fit3, k=log(n))
+	sp500_fit4 <- lm(sp_500 ~ . - gdp_us - cpi - m2, data=timeSeries)
+	summary(sp500_fit4)
+	drop1(sp500_fit4, k=log(n))
 
 # According to the drop1 summary, ppi has the lowest AIC value and will be dropped
 
-	sp_500fit4 <- lm(sp_500 ~ . - cpi - m2 - ppi, data=timeSeries)
-	summary(sp_500fit4)
-	drop1(sp_500fit4, k=log(n))
+	sp500_fit5 <- lm(sp_500 ~ . - gdp_us - cpi - m2 - ppi, data=timeSeries)
+	summary(sp500_fit5)
+	drop1(sp500_fit5, k=log(n))
 
 # According to the drop1 summary, customerSentiment has the lowest AIC value and will be dropped
 
-	sp_500fit5 <- lm(sp_500 ~ . - cpi - m2 - ppi - consumerSentiment, data=timeSeries)
-	summary(sp_500fit5)
-	drop1(sp_500fit5, k=log(n))
+	sp500_fit6 <- lm(sp_500 ~ . - gdp_us - cpi - m2 - ppi - consumerSentiment, data=timeSeries)
+	summary(sp500_fit6)
+	drop1(sp500_fit6, k=log(n))
 
 # inflation is the next variable with the lowest AIC value, so it will be dropped
 
-	sp_500fit6 <- lm(sp_500 ~ . - cpi - m2 - ppi - consumerSentiment - inflation, data=timeSeries)
-	summary(sp_500fit6)
-	drop1(sp_500fit6, k=log(n))
+	sp500_fit7 <- lm(sp_500 ~ . - gdp_us - cpi - m2 - ppi - consumerSentiment - inflation, data=timeSeries)
+	summary(sp500_fit7)
+	drop1(sp500_fit7, k=log(n))
 
 # Now the intercept value of AIC is lower than any other variable
 # Thus the model above is in terms of the most significant variables
@@ -161,16 +169,44 @@
 # Last we will analyze the nyse 
 # linear model on nyse as function of every economic indicator
 
-	nysefit <- lm(nyse ~ ., data = timeSeries)
+	nyse_fit1 <- lm(nyse ~ ., data = timeSeries)
 	print("Printing summary of initial regression on economic indicators vs NYSE ---------------------------------------------------")
-	summary(nysefit)
+	summary(nyse_fit1)
 
-# linear model on nyse as function of more influential economic indicators
-# variables with higher p values removed
+# We will use backwards elimination for the nyse
+# According to the summary, cpi has the highest p value
+# cpi will be dropped
 
-	nysefit2<- lm(nyse ~ m1 + imports + oilPrices + exports + cpi +  capUtilization + sp_500, data = timeSeries)
-	print("Printing summary of regression on more influential economic indicators vs NYSE ---------------------------------------------------")
-	summary(nysefit2)
+	nyse_fit2<- lm(nyse ~ . - cpi, data = timeSeries)
+	summary(nyse_fit2)
+
+# According to the summary, consumerSentiment has the highest p value and will be dropped
+
+	nyse_fit3<- lm(nyse ~ . - cpi - consumerSentiment, data = timeSeries)
+	summary(nyse_fit3)
+
+# According to the summary, sp_500 dividends have the highest p value and will be dropped
+
+	nyse_fit4<- lm(nyse ~ . - cpi - consumerSentiment - dividends, data = timeSeries)
+	summary(nyse_fit4)
+
+# According to the summary, imports have the highest p value and will be dropped
+
+	nyse_fit5<- lm(nyse ~ . - cpi - consumerSentiment - dividends - imports, data = timeSeries)
+	summary(nyse_fit5)
+
+# According to the summary, US GDP has the highest p value and will be removed
+
+	nyse_fit6<- lm(nyse ~ . - cpi - consumerSentiment - dividends - imports - gdp_us, data = timeSeries)
+	summary(nyse_fit6)
+
+# According to the summary, inflation has the highest p value and will be dropped
+
+	nyse_fit7<- lm(nyse ~ . - cpi - consumerSentiment - dividends - imports - gdp_us - inflation, data = timeSeries)
+	summary(nyse_fit7)
+
+# Now all of the variables have p values < 0.01
+# Therefore the model above is based solely on significant variables
 
 # Convert the timeSeries data into a dataframe
 
